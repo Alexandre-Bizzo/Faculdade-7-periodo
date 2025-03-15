2cc5179da0b6a3542b7e5e6385562fd965a677fc
Alexandre-Bizzo <127504549+Alexandre-Bizzo@users.noreply.github.com>
2025-03-14 21:46:49 -0300

    Add files via upload
    TDE 2 de qualidade de software

---
+++ Calculadora.java
@@ -0,0 +1,40 @@
+package controler;
+public class Calculadora {
+
+    /**
+     * calculadora.
+     * @param valor1
+     * @param valor2
+     * @return soma.
+     */
+    public double somar(final double valor1, final double valor2) {
+        return valor1 + valor2;
+    }
+    /**
+     * calculadora.
+     * @param valor1
+     * @param valor2
+     * @return subtrair.
+     */
+    public double subtrair(final double valor1, final double valor2) {
+        return valor1 - valor2;
+    }
+    /**
+     * calculadora.
+     * @param valor1
+     * @param valor2
+     * @return multiplicar.
+     */
+    public double multiplicar(final double valor1, final double valor2) {
+        return valor1 * valor2;
+    }
+    /**
+     * calculadora.
+     * @param valor1
+     * @param valor2
+     * @return dividir.
+     */
+    public double dividir(final double valor1, final double valor2) {
+        return valor1 / valor2;
+    }
+}
